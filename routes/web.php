1a1d18f2b9756834c25103b2f2cba91e7b2e3af0
ishikawakaito <k.ishikawa0323@icloud.com>
2022-07-06 12:27:27 +0900

    jissyu5

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,17 @@ Route::post('hello', 'HelloController@post');
 
 //実習
 Route::get('jissyu', 'JissyuController@index');
+//3-3
+Route::get('jissyu', 'Jissyu3_3Controller@Index');
+//5-1
+Route::get('jissyu10', 'Jissyu5_1Controller@index');
+Route::get('jissyu10/show', 'Jissyu5_1Controller@show');
+//5-2
+Route::get('jissyu11', 'Jissyu5_2Controller@index');
+Route::get('jissyu11/show', 'Jissyu5_2Controller@show');
+Route::get('jissyu11/add', 'Jissyu5_2Controller@add');
+Route::post('jissyu11/create', 'Jissyu5_2Controller@create');
+Route::get('jissyu11/edit', 'Jissyu5_2Controller@edit');
+Route::post('jissyu11/update', 'Jissyu5_2Controller@update');
+Route::get('jissyu11/del', 'Jissyu5_2Controller@del');
+Route::post('jissyu11/remove', 'Jissyu5_2Controller@remove');
